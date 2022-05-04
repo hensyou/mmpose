7325d4c271d0b34f692f3d67ddfeff349c5dd140
xiao <hensyou@gmail.com>
2022-05-03 21:52:34 -0400

    trying to get keypoints as json

--- demo/body3d_two_stage_video_demo.py
+++ demo/body3d_two_stage_video_demo.py
@@ -336,7 +336,7 @@ class NumpyEncoder(json.JSONEncoder):
         if isinstance(obj, np.ndarray):
             return obj.tolist()
         if isinstance(obj, np.float32) or isinstance(obj, np.float64):
-            return float(obj)
+            return round(float(obj),4)
         return json.JSONEncoder.default(self, obj)
 
 
